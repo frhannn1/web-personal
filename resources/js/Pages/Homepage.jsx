e7bd91da1225fc63051a203fa7de5dabb1c6716c
Farhan <farhanainurrahman2147@gmail.com>
2024-12-04 01:54:27 +0700

    bismillah

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function Homepage(){
+    return(
+        <>
+        <Head title="Welcome"/>
+        <h1>FARHAN GANTENG</h1>
+        </>
+        
+
+    )
+}
